8c71045f44b46c63c5538aa4be51008344795bba
David Bureš <buresdv@gmail.com>
2026-05-17 00:21:37 +0200

    ~ Build Bump

--- Project.swift
+++ Project.swift
@@ -3,7 +3,7 @@ import ProjectDescription
 let settings = Environment.selfCompiled.getBoolean(default: false)
 
 let version: String = "2.0.0"
-let build: String = "113DEV"
+let build: String = "114DEV"
 
 func corkTarget(configureWithSelfCompiled: Bool) -> ProjectDescription.Target {
     var additionalCompilationConditions = [String]()
